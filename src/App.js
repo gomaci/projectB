cadc26d4109b8d74692c7d17c771b054dd67d2a4
gomaci75 <gomaci75@gmail.com>
2025-06-05 20:16:29 +0900

    first commit3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+//yarn add react-router-dom@5
+
+function App() {
+  return (
+    <div>
+     <h3 >테스트06</h3>
+    </div>
+  );
+}
+
+export default App;
